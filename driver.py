81d8f8d1cb803476997e591965c33839bcdd55ac
Mohan Agrawal <mohan.agr97@gmail.com>
2025-06-04 17:14:31 -0400

    add driver code and sim results

--- driver.py
+++ driver.py
@@ -7,7 +7,7 @@ import time
 
 re=gn.narrowband(df=0.48,length=2048,up=1000) #conversion is 0.4 * N (0.4 is fraction of nyquist = N/2)
 im=gn.narrowband(df=0.48,length=2048,up=1000)
-
+lblock=4096
 nlevels=13
 up = 10
 f2 = 1 / 2
@@ -25,52 +25,59 @@ ybig2 = np.empty(nsamp,dtype='float64')
 csum = np.empty(nsamp,dtype='float64')
 clock = gf.flicker(nlevels, nsamp, f1, f2)
 nspec_per_run = nsamp//4096
-niter=5
+niter=100
 start_chan=1830
 nchans=10
 spectra1 = np.empty((nspec_per_run*niter,nchans),dtype='complex128')
 spectra2=spectra1.copy()
 delays = np.empty(nspec_per_run*niter,dtype=np.float64)
 # delays2 = np.empty(nsamp*niter,dtype=np.float64)
-for ii in range(niter):
-    t1=time.time()
-    re.generate().osamp()
-    im.generate().osamp()
-    clock.generate()
-    delay = gf.cumsum(csum,clock.ybig,start_delay,10**(-clock.nlevels/2))
-    start_delay=delay[-1]
-    # delays2[ii*nsamp:(ii+1)*nsamp]=delay
-    # print("first val of clock drift is", clock.ybig[0])
-    gn.get_delay(t_new,t_orig,delay)
-    # plt.plot(t_new-t_orig)
-    # plt.plot()
+delay = -5.5*np.ones(nsamp)
+tag='const_delay'
+try:
+    for ii in range(niter):
+        t1=time.time()
+        re.generate().osamp()
+        im.generate().osamp()
+        # clock.generate()
+        # delay = gf.cumsum(csum,clock.ybig,start_delay,10**(-clock.nlevels/2))
+        # start_delay=delay[-1]
+        # delays2[ii*nsamp:(ii+1)*nsamp]=delay
+        # print("first val of clock drift is", clock.ybig[0])
+        gn.get_delay(t_new,t_orig,delay)
+        # plt.plot(t_new-t_orig)
+        # plt.plot()
+        # plt.show()
+        carrier = 0.4478 #this is now for the oversampled one, 1835/4096
+        
+        I=re.ybig
+        Q=im.ybig
+        gn.upconvert_delay_noise(ybig,I,Q,t_orig,carrier,0.05) #last argument is sigma of noise you wanna add
+        I=gn.cubic_spline(t_new, t_orig, re.ybig)
+        Q=gn.cubic_spline(t_new, t_orig, im.ybig)
+        gn.upconvert_delay_noise(ybig2,I,Q,t_new,carrier,0.05)
+        obj1.pfb(ybig)
+        obj2.pfb(ybig2)
+        #save data
+        spectra1[ii*nspec_per_run:(ii+1)*nspec_per_run,:]=obj1.spectra[:,start_chan:start_chan+nchans]
+        spectra2[ii*nspec_per_run:(ii+1)*nspec_per_run,:]=obj2.spectra[:,start_chan:start_chan+nchans]
+        delays[ii*nspec_per_run:(ii+1)*nspec_per_run] = np.mean(np.reshape(delay,(-1,4096)),axis=1)
+        print(np.mean(np.reshape(delay,(-1,4096)),axis=1))
+        t2=time.time()
+        print(ii, t2-t1)
+        # xc=np.abs(obj1.spectra)**2#*np.conj(obj2.spectra)
+        # # print(xc.shape)
+        # xc=np.mean(xc,axis=0)
+        # plt.plot(np.abs(xc)[0:])
+        # plt.show()
+        # plt.plot(np.angle(xc)[0:])
+        # plt.show()
+    # plt.plot(delays2)
     # plt.show()
-    carrier = 0.4478 #this is now for the oversampled one, 1835/4096
-    
-    I=re.ybig
-    Q=im.ybig
-    gn.upconvert_delay_noise(ybig,I,Q,t_orig,carrier,0.01) #last argument is sigma of noise you wanna add
-    I=gn.cubic_spline(t_new, t_orig, re.ybig)
-    Q=gn.cubic_spline(t_new, t_orig, im.ybig)
-    gn.upconvert_delay_noise(ybig2,I,Q,t_new,carrier,0.01)
-    obj1.pfb(ybig)
-    obj2.pfb(ybig2)
-    #save data
-    spectra1[ii*nspec_per_run:(ii+1)*nspec_per_run,:]=obj1.spectra[:,start_chan:start_chan+nchans]
-    spectra2[ii*nspec_per_run:(ii+1)*nspec_per_run,:]=obj2.spectra[:,start_chan:start_chan+nchans]
-    delays[ii*nspec_per_run:(ii+1)*nspec_per_run] = np.mean(np.reshape(delay,(-1,4096)),axis=1)
-    t2=time.time()
-    print(t2-t1)
-    # xc=np.abs(obj1.spectra)**2#*np.conj(obj2.spectra)
-    # print(xc.shape)
-    # xc=np.mean(xc,axis=0)
-    # plt.plot(np.abs(xc)[0:])
-    # plt.show()
-    # plt.plot(np.angle(xc)[0:])
-    # plt.show()
-plt.plot(delays2)
-plt.show()
-
+except Exception as e:
+    pass
+finally:
+    np.savez(f"./spectra_{start_chan}_{start_chan+nchans}_{lblock}_{tag}.npz",spectra1=spectra1,spectra2=spectra2,delays=delays)
 
 
 
